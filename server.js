5cf64b82799813d565c8d65885d4c385d27c2a39
大阳 <Feliz337@outlook.com>
2020-09-07 21:29:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,21 +19,70 @@ mqttClient.on("message", ((topic, payload, packet) => {
     console.log(`topic: ${topic}, payload: ${payload}, packet ${packet}`);
 }));
 
+let switch01BurnOut = false;
+let switch02BurnOut = false;
+
+let CoolDownTime = 5000
+
+
 
 const express = require("express");
 
 const app = express();
 
-app.get("/on", ((req, res) => {
-    mqttClient.publish("NEKO/SWITCH/01", "on");
-    res.status(200).send("ON");
+app.get("/SWITCH/01/on", ((req, res) => {
+    if (!switch01BurnOut) {
+        switch01BurnOut = true;
+        mqttClient.publish("NEKO/SWITCH/01", "on");
+        res.status(200).send("SUCCESS,01ON");
+
+        setTimeout(() => switch01BurnOut = false, CoolDownTime);
+    } else {
+        res.status(400).send(`BurnOut,${switch01BurnOut}`);
+    }
+
 }));
 
-app.get("/off", ((req, res) => {
-    mqttClient.publish("NEKO/SWITCH/01", "off");
-    res.status(200).send("OFF");
+app.get("/SWITCH/01/off", ((req, res) => {
+    if (!switch01BurnOut) {
+        switch01BurnOut = true;
+        mqttClient.publish("NEKO/SWITCH/01", "off");
+        res.status(200).send("SUCCESS,01OFF");
+
+        setTimeout(() => switch01BurnOut = false, CoolDownTime);
+    } else {
+        res.status(400).send(`BurnOut,${switch01BurnOut}`);
+    }
+
+}));
+
+app.get("/SWITCH/02/on", ((req, res) => {
+    if (!switch02BurnOut) {
+        switch02BurnOut = true;
+        mqttClient.publish("NEKO/SWITCH/02", "on");
+        res.status(200).send("SUCCESS,02ON");
+
+        setTimeout(() => switch02BurnOut = false, CoolDownTime);
+    } else {
+        res.status(400).send(`BurnOut,${switch01BurnOut}`);
+    }
+
+}));
+
+
+app.get("/SWITCH/02/off", ((req, res) => {
+    if (!switch02BurnOut) {
+        switch02BurnOut = true;
+        mqttClient.publish("NEKO/SWITCH/02", "off");
+        res.status(200).send("SUCCESS,02OFF");
+
+        setTimeout(() => switch02BurnOut = false, CoolDownTime);
+    } else {
+        res.status(400).send(`BurnOut,${switch01BurnOut}`);
+    }
 }));
 
 app.listen(4009, () => {
     console.log("HTTP server listening on 4009");
 });
+
